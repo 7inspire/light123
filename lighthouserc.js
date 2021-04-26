69ef927ea6f72b66bdaf6c2da90514422ebdf1b8
Chang <tongchangsheng@bestpay.com.cn>
2021-04-26 18:45:28 +0800

    lighthouserc #1

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,11 +2,18 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './dist',
+      settings: {
+        configPath: './config.js'
+      }
     },
+    // assert: {
+    //   "preset": "lighthouse:recommended",
+    // },
     upload: {
       // target: 'filesystem',
       "target": "lhci",
-      "serverBaseUrl": "http://192.168.147.234:9001"
+      "serverBaseUrl": "http://192.168.147.234:9001",
+      token: '804bcd65-3c08-4488-9950-50a579420c6f'
     },
   },
 };
