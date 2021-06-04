6e317e6c9b9905f0b01708f727a2d4beb8e2f3b2
Chang <tongchangsheng@bestpay.com.cn>
2021-06-04 14:44:10 +0800

    update lh config

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,18 +3,24 @@ module.exports = {
     collect: {
       staticDistDir: './dist',
       settings: {
-        configPath: './config.js'
+        // configPath: './config.js',
+        chromeFlags: "--disable-gpu --no-sandbox",
+        onlyCategories: ['performance'],
+        locale: 'zh',
+        maxWaitForFcp: 15 * 1000,
+        maxWaitForLoad: 35 * 1000,
+        skipAudits: ['uses-http2']
       }
     },
     // assert: {
     //   "preset": "lighthouse:recommended",
     // },
     upload: {
-      target: 'temporary-public-storage',
+      // target: 'temporary-public-storage',
       // target: 'filesystem',
-      // "target": "lhci",
-      // "serverBaseUrl": "http://192.168.147.234:9001",
-      // token: '804bcd65-3c08-4488-9950-50a579420c6f'
+      "target": "lhci",
+      "serverBaseUrl": "http://127.0.0.1:9001",
+      token: '6b0f4ea1-a6a4-438c-83c1-56d5041c6340'
     },
   },
 };
